bb79b3126375b8fdaf477b3dd8301600156baad7
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-07-20 14:51:57 +0300

    Create 0006-zigzag-conversion.py

---
+++ 0006-zigzag-conversion.py
@@ -0,0 +1,17 @@
+from math import ceil
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        
+        n = len(s)
+        output = []
+        for r in range(numRows):
+            increment = 2*numRows-2
+            for i in range(r, n, increment):
+                output.append(s[i])
+                if 0<r<numRows-1 and i+increment-2*r < n:
+                    output.append(s[i+increment-2*r])
+                    
+            
+        return ''.join(output)
